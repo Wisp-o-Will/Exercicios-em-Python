7530fcbd75585aba3eacb8c64853277429d063cd
Wisp-o-Will <129694293+Wisp-o-Will@users.noreply.github.com>
2023-04-05 08:08:47 -0300

    Add files via upload

---
+++ Estrutura Sequencial/5.Conversão.py
@@ -0,0 +1,5 @@
+#Faça um Programa que converta metros para centímetros.
+
+metros = float(input("Informe o valor a ser convertido (m para cm):"))
+centimetros = metros * 100
+print(f"{metros}m é igual a {centimetros}cm")
---
+++ Estrutura Sequencial/7.Area do Quadrado.py
@@ -0,0 +1,7 @@
+#Faça um Programa que calcule a área de um quadrado, 
+#em seguida mostre o dobro desta área para o usuário.
+
+lado = int(input("Informe o valor do lado do quadrado:"))
+area_quadrado = lado * lado
+
+print(f"A área do quadrado é {area_quadrado} e o dobro dela é {2*area_quadrado}")
